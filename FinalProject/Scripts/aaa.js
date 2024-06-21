126010e34cd541162c27eff19fbbce1903400c0a
MOKOJOPE <momoinbb21@gmail.com>
2024-06-21 08:25:43 -1000

    Updated

--- FinalProject/Scripts/aaa.js
+++ FinalProject/Scripts/aaa.js
@@ -36,3 +36,5 @@ document.getElementById('registrationForm').addEventListener('submit', function(
         document.getElementById('registrationFeedback').style.display = 'none';
     }
 });
+
+//This is without server!!!
